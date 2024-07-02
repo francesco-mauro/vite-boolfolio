7495e8328fcf5187aef1a37beab53f45a52c135e
francesco-mauro <francesco.mauro180@gmail.com>
2024-07-02 16:08:04 +0200

    feat: working get from my server

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Projects</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/projects')
+      .then(response => {
+        this.projects = response.data
+      })
+      .catch(error => {
+        console.error('Error fetching projects:', error)
+      })
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
